7853481ff86700d4e8ea63b4549eab364d3fb7f5
VathanakSol <vathanaksol1605@gmail.com>
2025-06-05 14:30:18 +0700

    first init

---
+++ js/index.js
@@ -0,0 +1,15 @@
+// JavaScript File (index.js)
+console.log("JavaScript loaded from external file!");
+
+let message = "Hello, world!";
+console.log(message);
+
+// Single-Line Comment
+
+/*
+    Multi-Line Comment
+*/
+
+// Addition
+let result = 10 + 5;
+console.log(result);
